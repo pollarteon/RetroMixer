0a8554bcaa0acdf793e8b7c2122cd2dd5adb9a8e
Pollarteon <prafulph04@gmail.com>
2024-07-12 10:08:48 +0530

    deleted some audios 	deleted:    public/audio/niranjCOunt.mp3 	new file:   util/Colors.js 	deleted:    util/Colors.js

---
+++ util/Colors.js
@@ -0,0 +1,9 @@
+export const Colors = {
+  primary200:'#6fffe9',
+  primary400:'#5bc0be', 
+  primary500:'#065a60',
+  primary600:'#1b3a4b',
+  primary700:'#212f45',
+  primary800:'#1c2541',
+  primary900:'#0b132b',
+}
